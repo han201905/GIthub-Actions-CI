db75764f9b90a2bb498f4db970d82afadf33116a
han201905 <hanze@oregonstate.edu>
2021-06-07 05:40:58 -0700

    added multiply and divide tests

--- test.py
+++ test.py
@@ -9,6 +9,12 @@ class TestCase(unittest.TestCase):
     def test_sub_1(self):
         self.assertEqual(example.subtract(1, 1), 0)
 
+    def test_mul_1(self):
+        self.assertEqual(example.mul(2, 5), 10)
+
+    def test_div_1(self):
+        self.assertEqual(example.div(5, 2), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
