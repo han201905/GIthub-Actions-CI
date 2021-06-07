d97103680cf96be3fd6b3f7a966c48ad96d996eb
han201905 <hanze@oregonstate.edu>
2021-06-07 05:43:11 -0700

    added code to pass multiply and divide tests

--- test.py
+++ test.py
@@ -10,10 +10,10 @@ class TestCase(unittest.TestCase):
         self.assertEqual(example.subtract(1, 1), 0)
 
     def test_mul_1(self):
-        self.assertEqual(example.mul(2, 5), 10)
+        self.assertEqual(example.multiply(2, 5), 10)
 
     def test_div_1(self):
-        self.assertEqual(example.div(5, 2), 2)
+        self.assertEqual(example.divide(5, 2), 2)
 
 
 if __name__ == '__main__':
